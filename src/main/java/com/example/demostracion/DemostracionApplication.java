fd65f1c878e7d69ce6dd30c9d2a61222db6edd3e
pedro1151 <pedroramosfine@gmail.com>
2026-05-29 14:19:39 -0400

    first commit

---
+++ src/main/java/com/example/demostracion/DemostracionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demostracion/DemostracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demostracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemostracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
